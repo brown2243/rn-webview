2fa21e7965649eb0e99524173d31461507d4d3e3
brown <brown2243@naver.com>
2024-05-06 00:37:51 +0900

    chore: emotion styled 경로

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -1,8 +1,15 @@
+import styled from '@emotion/native';
 import React from 'react';
 import {Text} from 'react-native';
 
+const StyledText = styled(Text)`
+  color: red;
+  font-size: 16px;
+  font-weight: 700;
+`;
+
 function Test() {
-  return <Text>Test App!!!!!</Text>;
+  return <StyledText>Test App!!!!!</StyledText>;
 }
 
 export default Test;
---
+++ src/styles/emotion.d.ts
@@ -0,0 +1,5 @@
+import styled from '@emotion/native';
+
+declare module '@emotion/native' {
+  export default styled;
+}
